262acd7e13a291748359c1774cbbf05df8bc7a39
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-28 13:00:00 -0800

    post set up

--- pages/products.js
+++ pages/products.js
@@ -1,7 +1,30 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
+import NewProduct from "./products/new";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function products() {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        axios.get('/products').then(response => {
+            setProducts(response.data);
+        })
+    }, []);
     return (
-        <Layout>products page</Layout>
+        <Layout>
+            <Link className="bgblue-900 text-white rounded-md py-1 px-2" href={'/products/new'}>Add new product</Link>
+            <table className="basic">
+                <thead>
+                    <tr>
+                        <td>Product Name</td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </Layout>
     )
 }
